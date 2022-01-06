fba4a268f2cf23479f1473bb822dc6f01cc91bfd
Scarletleaph <gbipali@gmail.com>
2022-01-06 11:57:56 +0530

    Website added

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ var mysql = require('mysql2');
 var express = require('express');
 const connection = require('./databse'); 
 var app = express();
-app.get('/',function(req,res){
+
+app.use(express.static('./Website'));
+
+app.get('/all_product_info',function(req,res){
     let sql = 'Select * from Product;';
     connection.query(sql, function(err, results){
         if(err) throw err;
         res.send(results);
+
     });
     
 });
 app.listen(3000, function(){
     console.log("Listening to 3000");
-    connection.connect(function(err){   // whats connect?
+    connection.connect(function(err){   
         if(err) throw err;
         console.log('Connected');
     })
